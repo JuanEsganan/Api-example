6c2b31adc814624fa3b176ce08df75119aabc390
Juan Ganan <j_aness@hotmail.com>
2022-12-09 02:48:53 -0500

    Validations_query

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import Optional
 from pydantic import BaseModel
 # fastApi
 from fastapi import FastAPI
-from fastapi import Body    
+from fastapi import Body, Query    
 app = FastAPI()
 
 #Models
@@ -25,3 +25,13 @@ def home():
 @app.post("/person/new")
 def create_person (person: Person = Body (...)):
     return person
+
+#Validaciones: Query parameters
+
+@app.get ("/person/detail")
+def show_person (
+    name: Optional [str] = Query (default=None, min_length=1, max_length=50),
+    age: str = Query (...)
+    ):
+    return {name: age}
+
