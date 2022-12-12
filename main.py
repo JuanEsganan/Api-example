2773c9955011f5595f8d325eec48a26cea015d02
Juan Ganan <j_aness@hotmail.com>
2022-12-12 02:13:45 -0500

    files

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from enum import Enum
 #pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr
 # fastApi
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 from fastapi import status
 
 
@@ -175,3 +176,41 @@ def update_person (
 
 def login (username: str=Form (...), password: str = Form (...)):
     return LoginOut (username=username)
+
+#cookie and header parameters
+
+@app.post (
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact (
+    first_name: str = Form (
+        ...,
+        max_length=20,
+        min_length=1,
+        ),
+    last_name: str = Form (
+        ...,
+        max_length=20,
+        min_length=1),
+    email: EmailStr = Form (...),
+    message: str = Form (...,
+    min_length=20
+    ),
+    user_agent: Optional [str] = Header (default=None),
+    ads: Optional [str] = Cookie (default=None)
+):
+    return user_agent
+
+
+@app.post (
+    path="/post-image",
+)
+def post_image (
+    image: UploadFile = File (...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round (len (image.file.read ())/1024, ndigits=2)
+    }
